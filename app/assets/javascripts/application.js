44b43f51f59304354409dad8cc4fea01df5ec427
zachnewburgh <zach.newburgh@gmail.com>
2016-11-11 12:56:46 -0500

    Added JS controls on the videos to autoplay, mute/unmute onmouseover; added hidden divs with more information about each video that'll display on video mouseover

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,55 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+var player1;
+var player2;
+var player3;
+var player4;
+var player5;
+var player6;
+var player7;
+var player8;
+var player9;
+
+function onYouTubeIframeAPIReady() {
+  player1 = new YT.Player('player1', {events: {'onReady': onPlayerReady}});
+  player2 = new YT.Player('player2', {events: {'onReady': onPlayerReady}});
+  player3 = new YT.Player('player3', {events: {'onReady': onPlayerReady}});
+  player4 = new YT.Player('player4', {events: {'onReady': onPlayerReady}});
+  player5 = new YT.Player('player5', {events: {'onReady': onPlayerReady}});
+  player6 = new YT.Player('player6', {events: {'onReady': onPlayerReady}});
+  player7 = new YT.Player('player7', {events: {'onReady': onPlayerReady}});
+  player8 = new YT.Player('player8', {events: {'onReady': onPlayerReady}});
+  player9 = new YT.Player('player9', {events: {'onReady': onPlayerReady}});
+};
+
+function onPlayerReady() {
+  player1.mute();
+  player2.mute();
+  player3.mute();
+  player4.mute();
+  player5.mute();
+  player6.mute();
+  player7.mute();
+  player8.mute();
+  player9.mute();
+
+  player1.playVideo();
+  player2.playVideo();
+  player3.playVideo();
+  player4.playVideo();
+  player5.playVideo();
+  player6.playVideo();
+  player7.playVideo();
+  player8.playVideo();
+  player9.playVideo();
+}
+
+function showDiv(divId) {
+ document.getElementById(divId).style.display = "block";
+}
+
+function hideDiv(divId) {
+ document.getElementById(divId).style.display = "none";
+}
